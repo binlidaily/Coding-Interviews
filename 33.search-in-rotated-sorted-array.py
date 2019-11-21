f4e45af4d3e596acc412bfb91c807e47c5c1f4e1
binlidaily <binlidaily@gmail.com>
2019-11-21 14:23:05 +0800

    leetcode 33

--- 33.search-in-rotated-sorted-array.py
+++ 33.search-in-rotated-sorted-array.py
@@ -5,8 +5,31 @@
 #
 
 # @lc code=start
+# Time: O(n)
+# Space: O(1)
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
+        if not nums or len(nums) == 0:
+            return -1
+        n = len(nums)
+        l, r = 0, n-1
+        while l <= r:
+            mid = (l + r) >> 1
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < nums[r]:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            else:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
+# 196/196 cases passed (44 ms)
+# Your runtime beats 83.98 % of python3 submissions
+# Your memory usage beats 100 % of python3 submissions (12.8 MB)
 # @lc code=end
 
