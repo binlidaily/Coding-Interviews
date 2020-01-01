f2c7502f668285ee1ae8a48bd13d9d5244cdccff
binlidaily <binlidaily@gmail.com>
2020-01-01 15:50:08 +0800

    237

---
+++ 237.delete-node-in-a-linked-list.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=237 lang=python3
+#
+# [237] Delete Node in a Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+# 41/41 cases passed (24 ms)
+# Your runtime beats 99.69 % of python3 submissions
+# Your memory usage beats 100 % of python3 submissions (12.9 MB)
+# @lc code=end
+
