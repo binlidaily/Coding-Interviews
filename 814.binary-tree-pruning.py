95caf844dd1f76e57f41e27e8ab89687b088f0c2
binlidaily <binlidaily@gmail.com>
2020-01-05 17:23:32 +0800

    814

---
+++ 814.binary-tree-pruning.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=814 lang=python3
+#
+# [814] Binary Tree Pruning
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        if root.left:
+            root.left = self.pruneTree(root.left)
+        if root.right:
+            root.right = self.pruneTree(root.right)
+        if not root.left and not root.right and root.val == 0:
+            return None
+        return root
+
+# @lc code=end
+
