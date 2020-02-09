27e7af8073a6984d679e82da60271d00f98528c6
binlidaily <binlidaily@gmail.com>
2020-02-09 13:36:51 +0800

    1108

---
+++ 1108.defanging-an-ip-address.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=1108 lang=python3
+#
+# [1108] Defanging an IP Address
+#
+
+# @lc code=start
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        arr = list(address)
+        n = len(arr)
+        for i in range(n):
+            if arr[i] == '.':
+                arr[i] = '[.]'
+        return ''.join(arr)
+# 62/62 cases passed (28 ms)
+# Your runtime beats 57.53 % of python3 submissions
+# Your memory usage beats 100 % of python3 submissions (12.8 MB)
+# @lc code=end
+
+print(Solution().defangIPaddr("1.1.1.1"))
