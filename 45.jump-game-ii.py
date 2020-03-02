a9dfff8eb0fec3ae9312a8368ecef4ef576eb519
binlidaily <binlidaily@gmail.com>
2020-03-02 20:00:39 +0800

    45

--- 45.jump-game-ii.py
+++ 45.jump-game-ii.py
@@ -25,23 +25,40 @@ from typing import List
 
 # Time: O(nm)
 # Space: O(n)
-class Solution:
-    def jump(self, nums: List[int]) -> int:
-        n = len(nums)
-        jumps = 0
-        # these two variable store the index which it can reach
-        pre_jump_max, cur_jump_max = 0, 0
-        for i in range(n):
-            cur_jump_max = max(cur_jump_max, i + nums[i])
-            if pre_jump_max == n - 1:
-                break
-            if i == pre_jump_max:
-                jumps += 1
-                pre_jump_max = cur_jump_max
-        return jumps
+# class Solution:
+#     def jump(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         jumps = 0
+#         # these two variable store the index which it can reach
+#         pre_jump_max, cur_jump_max = 0, 0
+#         for i in range(n):
+#             cur_jump_max = max(cur_jump_max, i + nums[i])
+#             if pre_jump_max == n - 1:
+#                 break
+#             if i == pre_jump_max:
+#                 jumps += 1
+#                 pre_jump_max = cur_jump_max
+#         return jumps
 
 # 92/92 cases passed (96 ms)
 # Your runtime beats 78.9 % of python3 submissions
+# Your memory usage beats 8.33 % of python3 submissions (14.9 MB)
+
+# Time: O(n)
+# Space: O(1)
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        res = 0
+        pre_max_reach, cur_max_reach = 0, 0
+        for i, num in enumerate(nums):
+            if i > pre_max_reach:
+                pre_max_reach = cur_max_reach
+                res += 1
+            cur_max_reach = max(cur_max_reach, i + num)
+        return res
+
+# 92/92 cases passed (92 ms)
+# Your runtime beats 91.37 % of python3 submissions
 # Your memory usage beats 8.33 % of python3 submissions (14.9 MB)
 # @lc code=end
 
