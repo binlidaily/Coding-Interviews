ebdf4ffbf5a847879cf50acb6190e8f9209005a6
binlidaily <binlidaily@gmail.com>
2020-03-08 16:28:21 +0800

    69

--- LeetCode/69.sqrt-x.py
+++ LeetCode/69.sqrt-x.py
@@ -56,12 +56,29 @@ class Solution:
             elif x_cmp > x:
                 right = mid - 1
             else:
-                left = mid
-        return left
+                left = mid + 1
+        return right
 
 # 1017/1017 cases passed (28 ms)
 # Your runtime beats 90.13 % of python3 submissions
 # Your memory usage beats 100 % of python3 submissions (12.6 MB)
+
+class Solution1:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+
+        left, right = 1, x // 2
+        while left <= right:
+            mid = left + (right - left) // 2
+            center = x / mid
+            if mid == center:
+                return mid
+            elif mid > center:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left - 1
 # @lc code=end
 
 print(Solution().mySqrt(4), 2)
